6bf500dbcf9583ed68d84e468c4ea83e8bbeb3cc
bo-sfl <bli@sflscientific.com>
2020-12-07 21:51:29 -0800

    change output to json str

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 
 echo "Activate Github in last $1 days."
 echo "The template getting checked is $2."
-time=$(python /src/main.py --last_active $1 --template_name $2 --token $3)
-echo "::set-output name=time::$time"
+repo_list=$(python /src/main.py --last_active $1 --template_name $2 --token $3)
+echo "::set-output name=repo_list::$repo_list"
