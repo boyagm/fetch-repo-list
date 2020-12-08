f167cddca94cba25ae29f137e7632e2e8ef3585b
bo-sfl <bli@sflscientific.com>
2020-12-07 16:53:16 -0800

    Fix docker entrypoint

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@ FROM python:3.8.6-slim
 
 RUN pip install --pre gql[all] --no-cache
 
-WORKDIR /src
 COPY . /src
 
 ENTRYPOINT ["/src/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 
 echo $1
 echo $2
-time=$(python main.py --last_active $1 --template_name $2)
+time=$(python /src/main.py --last_active $1 --template_name $2)
 echo "::set-output name=time::$time"
