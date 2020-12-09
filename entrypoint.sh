b5fce4aafc532898440f68a6334be8b8f99f99c5
bo-sfl <bli@sflscientific.com>
2020-12-09 15:21:51 -0800

    fix bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 echo "Activate Github in last $1 days."
 echo "The template getting checked is $2."
 python /src/main.py --last_active $1 --template_name $2 --token $3
-echo "::set-output name=repo_list::$(cat /src/repos.txt)"
+echo "::set-output name=repo_list::$(cat repos.txt)"
